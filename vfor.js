611e434c42227e8c2a3f5d2269a9818ed59ab02c
unknown <jerateep_@live.com>
2019-08-26 00:40:01 +0700

    vfor

--- vfor.js
+++ vfor.js
@@ -1,9 +1,15 @@
 new Vue({
     el:"#vue-app",
     data:{
-        like:false
+        like:false,
+        product:['CPU','RAM','VGA','HDMI','Keyboard'],
+        employee:[
+            {name:'A',age:30,status:false},
+            {name:'B',age:32,status:true},
+            {name:'C',age:50,status:false},
+        ]
     },
     methods:{
-        
+
       }
   })
